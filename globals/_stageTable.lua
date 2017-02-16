497deb0aeb47fa9982ece7027145709e10d50ac3
Kyle Stevens <Kyle Stevens>
2017-02-16 07:06:43 -0800

    lets do this

--- globals/_stageTable.lua
+++ globals/_stageTable.lua
@@ -4,6 +4,6 @@ if not G then G = {} end
 
 G.stage = {
   --{name="Overworld", file="ow_test",type="ow", music="song_00"},
-  {name="Level 1", file="lvl_01", type="stage", music="song_00"},
+  {name="Level 1", file="stage_1_1", type="stage", music="song_00"},
   {name="Arena", file="lvl_02", type="stage", music="song_00"},
 }
