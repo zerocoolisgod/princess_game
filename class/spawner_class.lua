899ce88e5aa2c0703e545c446d168dd4eea7084a
zerocoolisgod <zero.cool.is.god@gmail.com>
2017-06-28 19:23:41 -0700

    more map stuff

--- class/spawner_class.lua
+++ class/spawner_class.lua
@@ -4,15 +4,10 @@ local Entity    = require "class.entity_class"
 local lgr = love.graphics.rectangle
 local lgsc = love.graphics.setColor
 
-local className = "Spawner Class"
-local entityId  = "spawner"
-local sizeX = 1
-local sizeY = 1
-
-local e = Class(className, Entity)
+local e = Class("Spawner Class", Entity)
 
 function e:initialize (x,y,type)
-  Entity.initialize (self,x,y,sizeX,sizeY,entityId)
+  Entity.initialize (self, x, y, 1, 1, "spawner")
   self.solid = false
   self.spawn_type = type
   self.spawn_delay = 5
@@ -22,26 +17,11 @@ function e:initialize (x,y,type)
 end
 
 function e:on_update_first(dt)
-  -- if self.first_spawn then
-  --   self.first_spawn = false
-  --   self.ent = G.resource_manager:get_new_object(self.spawn_type, self.pos.x, self.pos.y)
-  --   G.add_object(self.ent)
-  -- end
-
   if self.ent and self.ent.remove then self.ent = nil end
   self.timers.respawn = self.spawn_delay
 end
 
-function e:on_update_last(dt)
-  
-end
-
 function e:off_screen_update (dt)
-  -- if not self.ent and self.timers.respawn <=0 then
-  --   self.timers.respawn = self.spawn_delay
-  --   self.ent = G.resource_manager:get_new_object(self.spawn_type, self.pos.x, self.pos.y)
-  --   G.add_object(self.ent)
-  -- end
   self:update_timers(dt)
   if not self.ent and (self.timers.respawn <= 0) then
     self.ent = G.resource_manager:get_new_object(self.spawn_type, self.pos.x, self.pos.y)
@@ -49,12 +29,6 @@ function e:off_screen_update (dt)
   end
 end
 
--- function e:on_draw_first()
--- end
-
--- function e:on_collision ()
-  
--- end
 
 function e:draw_bounding_box()
   --draws a yellow box of self.size at self.position
@@ -70,6 +44,7 @@ function e:draw_bounding_box()
   lgr("line", x, y, w, h)
   lgsc(255, 255, 255,255)
 end
+
 
 function e:set_spawn_type(type)
   self.spawn_type = type
