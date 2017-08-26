daa5c2ac35b6a7f488389140670ad59ea9bc779a
zerocoolisgod <zero.cool.is.god@gmail.com>
2017-08-26 12:08:56 -0700

    Homing duck sprite work

--- entities/ent_duck_homing.lua
+++ entities/ent_duck_homing.lua
@@ -1,7 +1,7 @@
 -- run duck, basic enemy
 
 local Class     = require 'lib.middleclass'
-local Duck    = require "class.duck_class"
+local Duck      = require "class.duck_class"
 local Sprite    = require 'class.sprite_class'
 
 
@@ -9,7 +9,7 @@ local e = Class('duck_homing', Duck)
 function e:initialize (x,y)
   --init super class
   Duck.initialize(self,x,y,16,16,'duck_homing')
-  self.sprite = Sprite:new('duck homing sprite','duck_fly_sheet',16,16,0,0)
+  self.sprite = Sprite:new('duck homing sprite','duck_homing_sheet',16,16,0,0)
   self.sprite:add_animation('fly',{1,2,3,2},16)
   self.sprite:add_animation('death',{4,5},15)
   self.sprite:set_animation('fly')
@@ -26,7 +26,9 @@ function e:initialize (x,y)
   self.speed.y = 80
   self.timers.wait = 0
   self.timers.fly = 0
-  
+  self.wait_time = 1
+  self.fly_time = 0.6
+
   self.health = 1
   self.damage = 1
 
@@ -39,14 +41,14 @@ end
 function e:init_state (s)
   if s=="fly" then
     self:set_direction(G.direction_to_player(self.pos.x, self.pos.y))
-    self.accel.x = .08
-    self.accel.y = .08
-    self.timers.fly = .6
+    self.accel.x = 0.08
+    self.accel.y = 0.08
+    self.timers.fly = self.fly_time
   end
   if s=="wait" then
-    self.accel.x = .08
-    self.accel.y = .08
-    self.timers.wait = .5
+    self.accel.x = 0.1
+    self.accel.y = 0.1
+    self.timers.wait = self.wait_time
   end
 end
 
